0a78f1fa4c774b8256d99347a0b2af2de4bdef48
Willian Damasceno <williandamasceno700@gmail.com>
2021-06-20 14:26:52 -0300

    Fixing bugs

--- src/Components/hamburger.js
+++ src/Components/hamburger.js
@@ -1,8 +1,27 @@
-const hamburger = document.querySelector('.header__hamburger')
+const hamburgerDiv = document.querySelector('.header__hamburger')
+const hamburger = document.querySelector('.header__hamburger input')
 const mobileNavigation = document.querySelector('.mobile-navigation')
 
-hamburger.addEventListener('click', function() {
+hamburgerDiv.addEventListener('click', function () {
     const [hamburgerInput] = this.children
 
-    mobileNavigation.style.right = hamburgerInput.checked ? '0' : '-100%' 
+    mobileNavigation.style.right = hamburgerInput.checked ? '0px' : '-100%'
+
+    document.body.style.overflowY =
+        mobileNavigation.style.right === '0px' ? 'hidden' : 'auto'
+
+    var scrollTopIntervalId = setInterval(() => {
+        scrollY === 0
+            ? clearInterval(scrollTopIntervalId)
+            : scrollTo(0, scrollY - 1)
+    }, 1)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    hamburger.checked = false
+
+    mobileNavigation.style.right = '-100%'
+
+    document.body.style.overflowX = 'hidden'
+    document.body.style.overflowY = 'auto'
 })
